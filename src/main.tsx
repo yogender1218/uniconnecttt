82956e9e3d7baa7b96dc45ee3222d64e25229e73
yogender1218 <yyyyyy.123001@gmail.com>
2025-03-31 13:51:30 +0530

    dsd

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Ensure TypeScript knows "root" is not null
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+} else {
+  console.error("Root element not found");
+}
